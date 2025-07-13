8d1f724fa746684bfd484c8d81529a7ec2404282
Subhrajyoti <subhrajyotikundu96@gmail.com>
2025-07-13 23:13:44 +0530

    [subhrjyoti kundu].Add sorting for Day 17

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,40 @@
+public class QuickSort {
+    public static int partion(int []arr,int low,int high){
+        int pivot=arr[high];
+        int i=low-1;
+        for(int j=0;j<high-1;j++){
+            if(arr[j]<pivot){
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,i+1,pivot);
+        return i+1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+     public  static void quicksort(int []arr,int low,int high){
+        if(low<high){
+            int pi=partion(arr,low,high);
+            quicksort(arr,0,pi-1);
+            quicksort(arr,pi+1,high);
+        }
+     }
+
+    public static void main(String[] args) {
+
+            int[] arr = {10, 7, 8, 9, 1, 5};
+            int n = arr.length;
+
+            quicksort(arr,0,n-1);
+
+            for (int val : arr) {
+                System.out.print(val + " ");
+            }
+        }
+    }
+
